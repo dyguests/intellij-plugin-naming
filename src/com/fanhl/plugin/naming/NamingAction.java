a52dc08e2ae67400615e9419d131f403962458b0
fanhl <fanhonglin@klicen.com>
2017-11-01 16:13:56 +0800

    NamingAction

---
+++ src/com/fanhl/plugin/naming/NamingAction.java
@@ -0,0 +1,19 @@
+package com.fanhl.plugin.naming;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * 命名Action
+ * <p>
+ * date: 2017/11/01
+ *
+ * @author fanhl
+ */
+public class NamingAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
