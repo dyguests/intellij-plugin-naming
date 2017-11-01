f2e71555d7521db55d2bcdb4ee45f628d0d54c8d
fanhl <fanhonglin@klicen.com>
2017-11-01 16:43:59 +0800

    update

--- src/com/fanhl/plugin/naming/NamingAction.java
+++ src/com/fanhl/plugin/naming/NamingAction.java
@@ -5,7 +5,10 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
 
 /**
  * 命名Action
@@ -18,11 +21,29 @@ public class NamingAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
+        PsiClass psiClass = getPsiClass(e);
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        super.update(e);
+        e.getPresentation().setEnabled(getPsiClass(e) != null);
+    }
+
+    /**
+     * 获取action所在的class
+     *
+     * @param e e
+     * @return action所在的class
+     */
+    private PsiClass getPsiClass(AnActionEvent e) {
         PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
         Editor editor = e.getData(PlatformDataKeys.EDITOR);
         if (psiFile == null || editor == null) {
-            return;
+            return null;
         }
         int offset = editor.getCaretModel().getOffset();
+        PsiElement elementAtOffset = psiFile.findElementAt(offset);
+        return PsiTreeUtil.getParentOfType(elementAtOffset, PsiClass.class);
     }
 }
