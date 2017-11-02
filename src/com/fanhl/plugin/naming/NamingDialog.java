9e3948e42977fb5759f8b09bbf53f3a35bad0a40
fanhl <fanhonglin@klicen.com>
2017-11-02 09:21:41 +0800

    NamingDialog

--- src/com/fanhl/plugin/naming/NamingDialog.java
+++ src/com/fanhl/plugin/naming/NamingDialog.java
@@ -3,9 +3,11 @@ package com.fanhl.plugin.naming;
 import com.intellij.ide.util.DefaultPsiElementCellRenderer;
 import com.intellij.ide.util.PsiElementModuleRenderer;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.LabeledComponent;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiField;
 import com.intellij.ui.CollectionListModel;
+import com.intellij.ui.ToolbarDecorator;
 import com.intellij.ui.components.JBList;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,20 +19,27 @@ import javax.swing.*;
  * @author fanhl
  */
 public class NamingDialog extends DialogWrapper {
-
     private final CollectionListModel<PsiField> myFields;
+    private final LabeledComponent<JPanel> component;
 
     NamingDialog(PsiClass psiClass) {
         super(psiClass.getProject());
         setTitle("Select Fields to Rename");
+
         myFields = new CollectionListModel<>(psiClass.getAllFields());
         JBList<PsiField> fieldJBList = new JBList<>(myFields);
-        fieldJBList.setCellRenderer(new PsiElementModuleRenderer());
+        fieldJBList.setCellRenderer(new DefaultPsiElementCellRenderer());
+        ToolbarDecorator decorator = ToolbarDecorator.createDecorator(fieldJBList);
+        decorator.disableAddAction();
+        JPanel panel = decorator.createPanel();
+        component = LabeledComponent.create(panel, "Select Fields to Rename.");
+
+        init();
     }
 
     @Nullable
     @Override
     protected JComponent createCenterPanel() {
-        return null;
+        return component;
     }
 }
